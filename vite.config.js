4bffca88705fafa1e3a15e0f16ac2086b406595d
prajul-l850 <prajulgeorgefrancis@gmail.com>
2025-04-03 12:48:03 +0000

    Fix: Properly add vite-plugin-vercel

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import vercel from 'vite-plugin-vercel';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import vercel from 'vite-plugin-vercel'  // Ensure this is installed
 
-export default {
-  plugins: [vercel()],
-};
+export default defineConfig({
+  plugins: [react(), vercel()],
+});
