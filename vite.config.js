99940f046bf8ae307af55c4ed8159be0e90e0eca
prajul-l850 <prajulgeorgefrancis@gmail.com>
2025-04-03 12:41:09 +0000

    vercel2

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 import vercel from 'vite-plugin-vercel';
 
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),vercel()],
-});
+export default {
+  plugins: [vercel()],
+};
